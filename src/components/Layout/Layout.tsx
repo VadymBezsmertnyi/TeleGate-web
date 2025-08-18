b0cd65bbfcdad27bc903242c1af504bdce99250e
Vadym <vados199368@gmail.com>
2025-08-18 10:31:04 +0300

    Refactor: Simplify Layout component by removing unused providers

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,18 +2,9 @@ import React, { FunctionComponent } from "react";
 
 // providers
 import LocalesProvider from "@/localization/localization.provider";
-import ApiProvider from "@/providers/ApiProvider/ApiProvider";
-import UserProvider from "@/providers/UserProvider/UserProvider";
-import SubscriptionProvider from "@/providers/SubscriptionProvider/SubscriptionProvider";
-import BusinessProvider from "@/providers/BusinessProvider/BusinessProvider";
-import PlacesProvider from "@/providers/PlacesProvider/PlacesProvider";
-import WorkshopsProvider from "@/providers/WorkshopsProvider/WorkshopsProvider";
-import LoadingProvider from "@/providers/LoadingProvider/LoadingProvider";
-import MapProvider from "@/providers/MapProvider/MapProvider";
 
 // components
 import { Header } from "../Header/Header";
-import MarketProvider from "@/providers/MarketProvider/MarketProvider";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -22,28 +13,10 @@ type LayoutProps = {
 const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
   return (
     <LocalesProvider>
-      <LoadingProvider>
-        <ApiProvider>
-          <UserProvider>
-            <MapProvider>
-              <SubscriptionProvider>
-                <BusinessProvider>
-                  <PlacesProvider>
-                    <WorkshopsProvider>
-                      <MarketProvider>
-                        <>
-                          <Header />
-                          <main>{children}</main>
-                        </>
-                      </MarketProvider>
-                    </WorkshopsProvider>
-                  </PlacesProvider>
-                </BusinessProvider>
-              </SubscriptionProvider>
-            </MapProvider>
-          </UserProvider>
-        </ApiProvider>
-      </LoadingProvider>
+      <>
+        <Header />
+        <main>{children}</main>
+      </>
     </LocalesProvider>
   );
 };
